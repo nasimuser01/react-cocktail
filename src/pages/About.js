2e89a5c02aa69f0279a7278ba5793106ee244fdd
Nasim Jani <nasimuser01@gmail.com>
2022-09-08 09:53:02 +1000

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima optio cum, velit molestiae quos a ex mollitia rem quo placeat cumque, sint culpa inventore, illum saepe nobis eaque aliquid sequi?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque sit fuga porro maiores nulla excepturi asperiores eligendi ex, omnis sunt.
+      </p>
+    </section>
+  )
+}
+
+export default About
